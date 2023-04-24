7f907a78d193b09c880eb107342a93bd5af1c58d
Jude Payne <judepayne@yahoo.co.uk>
2023-04-24 19:45:55 +0100

    adding transform function

--- src/instaparse/core.bb
+++ src/instaparse/core.bb
@@ -4,9 +4,65 @@
 (pods/load-pod
  ;; for local dev:
  #_["clojure" "-Sdeps" "{:deps {ip/ip {:local/root \"/Users/borkdude/dev/pod-babashka-instaparse\"}}}" "-M" "-m" "pod.babashka.instaparse"]
- 'org.babashka/instaparse "0.0.2")
+ 'org.babashka/instaparse "0.0.3")
 
 (require '[pod.babashka.instaparse :as insta])
+
+(defn- map-preserving-meta [f l]
+  (with-meta (map f l) (meta l)))
+
+(defn- merge-meta-
+  "A variation on with-meta that merges the existing metamap into the new metamap,
+rather than overwriting the metamap entirely."
+  [obj metamap]
+  (with-meta obj (merge metamap (meta obj))))
+
+(defn- merge-meta
+  "This variation of the merge-meta in gll does nothing if obj is not
+something that can have a metamap attached."
+  [obj metamap]
+  (if (instance? clojure.lang.IObj obj)
+    (merge-meta- obj metamap)
+    obj))
+
+(defn- throw-illegal-argument-exception
+  [& message]
+  (let [^String text (apply str message)]
+    (-> text
+        IllegalArgumentException.
+        throw)))
+
+(defn- enlive-transform
+  [transform-map parse-tree]
+  (let [transform (transform-map (:tag parse-tree))]
+    (cond
+      transform
+      (merge-meta 
+       (apply transform (map (partial enlive-transform transform-map)
+                             (:content parse-tree)))
+       (meta parse-tree))
+      (:tag parse-tree)
+      (assoc parse-tree :content (map (partial enlive-transform transform-map)
+                                      (:content parse-tree)))
+      :else
+      parse-tree)))
+
+(defn- hiccup-transform
+  [transform-map parse-tree]
+  (if (and (sequential? parse-tree) (seq parse-tree))
+    (if-let [transform (transform-map (first parse-tree))]
+      (merge-meta
+        (apply transform (map (partial hiccup-transform transform-map)
+                              (next parse-tree)))
+        (meta parse-tree))
+      (with-meta 
+        (into [(first parse-tree)]
+              (map (partial hiccup-transform transform-map) 
+                   (next parse-tree)))
+        (meta parse-tree)))
+    parse-tree))
+
+;; Public functions
 
 (defn parser [& args]
   (apply insta/parser args))
@@ -16,3 +72,33 @@
 
 (defn failure? [& args]
   (apply insta/failure? args))
+
+(defn transform
+  "Replicates the `transform` function from instaparse."
+  [transform-map parse-tree]
+  ; Detect what kind of tree this is
+  (cond
+    (string? parse-tree)
+    ; This is a leaf of the tree that should pass through unchanged
+    parse-tree
+
+    (and (map? parse-tree) (:tag parse-tree))
+    ; This is an enlive tree-seq
+    (enlive-transform transform-map parse-tree)
+    
+    (and (vector? parse-tree) (keyword? (first parse-tree)))
+    ; This is a hiccup tree-seq
+    (hiccup-transform transform-map parse-tree)
+    
+    (sequential? parse-tree)
+    ; This is either a sequence of parse results, or a tree
+    ; with a hidden root tag.
+    (map-preserving-meta (partial transform transform-map) parse-tree)
+    
+    (insta/failure? parse-tree)
+    ; pass failures through unchanged
+    parse-tree
+    
+    :else
+    (throw-illegal-argument-exception
+      "Invalid parse-tree, not recognized as either enlive or hiccup format.")))
---
+++ test.clj
@@ -0,0 +1,72 @@
+#!/usr/bin/env bb
+
+(require '[instaparse.core :as insta]
+         '[clojure.string :as str])
+
+(def as-and-bs
+  (insta/parser
+   "S = AB*
+    AB = A B
+    A = 'a'+
+    B = 'b'+"))
+
+(assert (= [:S [:AB [:A "a" "a" "a" "a" "a"] [:B "b" "b" "b"]] [:AB [:A "a" "a" "a" "a"] [:B "b" "b"]]] (insta/parse as-and-bs "aaaaabbbaaaabb")))
+
+(def failure (insta/parse as-and-bs "xaaaaabbbaaaabb"))
+
+(assert (insta/failure? failure) "should be true")
+
+(def commit-msg-grammar
+  "A PEG grammar to validate and parse conventional commit messages."
+  (str
+    "<S>            =       (HEADER <EMPTY-LINE> FOOTER GIT-REPORT? <NEWLINE>*)
+                            / ( HEADER <EMPTY-LINE> BODY (<EMPTY-LINE> FOOTER)? GIT-REPORT? <NEWLINE>*)
+
+                            / (HEADER <EMPTY-LINE> BODY GIT-REPORT? <NEWLINE>*)
+                            / (HEADER GIT-REPORT? <NEWLINE>*);"
+    "<HEADER>       =       TYPE (<'('>SCOPE<')'>)? <':'> <SPACE> SUBJECT;"
+    "TYPE           =       'feat' | 'fix' | 'refactor' | 'perf' | 'style' | 'test' | 'docs' | 'build' | 'ops' | 'chore';"
+    "SCOPE          =       #'[a-zA-Z0-9]+';"
+    "SUBJECT        =       TEXT ISSUE-REF? TEXT? !'.';"
+    "BODY           =       (!PRE-FOOTER PARAGRAPH) / (!PRE-FOOTER PARAGRAPH (<EMPTY-LINE> PARAGRAPH)*);"
+    "PARAGRAPH      =       (ISSUE-REF / TEXT / (NEWLINE !NEWLINE))+;"
+    "PRE-FOOTER     =       NEWLINE+ FOOTER;"
+    "FOOTER         =       FOOTER-ELEMENT (<NEWLINE> FOOTER-ELEMENT)*;"
+    "FOOTER-ELEMENT =       FOOTER-TOKEN <':'> <WHITESPACE> FOOTER-VALUE;"
+    "FOOTER-TOKEN   =       ('BREAKING CHANGE' (<'('>SCOPE<')'>)?) / #'[a-zA-Z\\-^\\#]+';"
+    "FOOTER-VALUE   =       (ISSUE-REF / TEXT)+;"
+    "GIT-REPORT     =       (<EMPTY-LINE> / <NEWLINE>) COMMENT*;"
+    "COMMENT        =       <'#'> #'[^\\n]*' <NEWLINE?> ;"
+    "ISSUE-REF      =       <'#'> ISSUE-ID;"
+    "ISSUE-ID       =       #'([A-Z]+\\-)?[0-9]+';"
+    "TEXT           =       #'[^\\n\\#]+';"
+    "SPACE          =       ' ';"
+    "WHITESPACE     =       #'\\s';"
+    "NEWLINE        =       <'\n'>;"
+    "EMPTY-LINE     =       <'\n\n'>;"))
+
+(def commit-msg-parser-hiccup (insta/parser commit-msg-grammar))
+
+(def commit-msg-parser-enlive (insta/parser commit-msg-grammar :output-format :enlive))
+
+(assert (= '([:TYPE "feat"] [:SUBJECT [:TEXT "adding a new awesome feature"]])
+           (insta/parse commit-msg-parser-hiccup "feat: adding a new awesome feature")))
+
+(assert (= '("feat::" "ADDING A NEW AWESOME FEATURE")
+         (insta/transform {:TEXT reverse
+                           :SUBJECT (comp str/reverse str/upper-case str/join)
+                           :TYPE (fn [t] (str t "::"))}
+                       (insta/parse commit-msg-parser-hiccup "feat: adding a new awesome feature"))))
+
+;; test enlive
+
+(assert (= '({:tag :TYPE, :content ("feat")}
+ {:tag :SUBJECT,
+  :content ({:tag :TEXT, :content ("adding a new awesome feature")})})
+           (insta/parse commit-msg-parser-enlive "feat: adding a new awesome feature")))
+
+(assert (= '("feat::" "ADDING A NEW AWESOME FEATURE")
+         (insta/transform {:TEXT reverse
+                           :SUBJECT (comp str/reverse str/upper-case str/join)
+                           :TYPE (fn [t] (str t "::"))}
+                       (insta/parse commit-msg-parser-enlive "feat: adding a new awesome feature"))))
